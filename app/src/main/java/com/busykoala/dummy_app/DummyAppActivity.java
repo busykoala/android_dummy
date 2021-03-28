43695242d81e196584e89d9aeb1673ec2ffa205b
Matthias Osswald <m@osswald.li>
2021-03-28 15:06:20 +0200

    Add event listener and counter according to tutorial.

---
+++ app/src/main/java/com/busykoala/dummy_app/DummyAppActivity.java
@@ -0,0 +1,50 @@
+package com.busykoala.dummy_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DummyAppActivity extends AppCompatActivity {
+    private TextView message;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.clickCounter);
+        ImageView avatar = findViewById(R.id.avatarImage);
+
+        //Define and attach click listener
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tapAvatar();
+            }
+        });
+    }
+
+    private void tapAvatar() {
+        counter++;
+        String countAsText;
+        /*
+         * In real applications you should not write switch like the one below.
+         * Use resource of type "Quantity strings (plurals)" instead.
+         * See https://developer.android.com/guide/topics/resources/string-resource#Plurals
+         */
+        switch (counter) {
+            case 1:
+                countAsText = "once";
+                break;
+            case 2:
+                countAsText = "twice";
+                break;
+            default:
+                countAsText = String.format("%d times", counter);
+        }
+        message.setText(String.format("You touched the avatar %s", countAsText));
+    }
+}
